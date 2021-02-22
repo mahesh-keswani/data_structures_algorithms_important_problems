3c2344f62b0635115e4c6eb23e58cb7385ec3687
mahesh-keswani <karthikkeswani1234@gmail.com>
2021-02-22 22:41:40 +0530

    check if susbset with sum k is present

---
+++ knapsack_variations.py
@@ -0,0 +1,18 @@
+def checkSubsetWithSumK(array, k):
+    n = len(array)
+    dp = [ [False for sum in range(0, k + 1)] for size in range(0, n + 1)]
+
+    # As subset with sum 0 is always possible ( always take null set )
+    for i in range(0, n + 1):
+        dp[i][0] = 1
+
+    for row in range(1, n + 1):
+        for col in range(1, k + 1):
+            if array[row - 1] <= col:
+                dp[row][col] = dp[row - 1][col - array[row - 1]] or dp[row - 1][col]
+            else:
+                dp[row][col] = dp[row - 1][col]
+
+        
+    return dp[-1][-1]
+
