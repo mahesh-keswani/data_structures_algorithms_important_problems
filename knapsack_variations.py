f47a8938529bcdf8b0463d4f794cd6a3a7212324
mahesh-keswani <karthikkeswani1234@gmail.com>
2021-02-22 22:42:19 +0530

    count the subset with sum k

--- knapsack_variations.py
+++ knapsack_variations.py
@@ -16,3 +16,19 @@ def checkSubsetWithSumK(array, k):
         
     return dp[-1][-1]
 
+def countSubsetWithSumK(array, k):
+    n = len(array)
+    dp = [ [0 for sum in range(0, k + 1)] for size in range(0, n + 1)]
+
+    for i in range(0, n + 1):
+        dp[i][0] = 1
+
+    for row in range(1, n + 1):
+        for col in range(1, k + 1):
+            if array[row - 1] <= col:
+                dp[row][col] = dp[row - 1][col - array[row - 1]] + dp[row - 1][col]
+            else:
+                dp[row][col] = dp[row - 1][col]
+
+        
+    return dp[-1][-1]
