cee5dac7fd7d4b36b2aebe3e5a8a5d7d7901174c
mahesh-keswani <karthikkeswani1234@gmail.com>
2021-03-06 21:39:50 +0530

    explicitly adding loop in the list

--- linkedListProblems.py
+++ linkedListProblems.py
@@ -26,7 +26,29 @@ class SinglyLL:
         while node:
             print(node.value)
             node = node.next
-            
+
+    # staring -> starting of loop and ending => ending of loop 
+    def createLoop(self, starting, ending):
+        if self.head is None:
+            return
+
+        # For keeping track of starting and ending Nodes
+        startingNode = None
+        endingNode = None
+        
+        node = self.head
+        while node is not None:
+            if node.value == starting:
+                startingNode = node
+
+            if node.value == ending:
+                endingNode = node
+
+            node = node.next
+
+        # adding loop here
+        endingNode.next = startingNode
+                
 '''
     1-2-3-4-5-6
             |  |
@@ -37,4 +59,7 @@ singlyLL = SinglyLL()
 
 singlyLL.insert(1).insert(2).insert(3).insert(4).insert(5).insert(6).insert(7).insert(8)
 
-singlyLL.printList()
+# singlyLL.printList()
+
+singlyLL.createLoop(5, 8)
+# singlyLL.printList()
