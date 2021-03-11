25c3c73c3524c747e24ad3f13b7ae6d4aa57ef8d
mahesh-keswani <karthikkeswani1234@gmail.com>
2021-03-11 21:40:58 +0530

    reducing the number to 1 in min number of steps optimal approach

--- reduceNumberTo1.py
+++ reduceNumberTo1.py
@@ -12,3 +12,32 @@ def reduceNumberTo1Naive(n):
         return 1 + reduceNumberTo1Naive(n // 2)
     # else count the number of steps for both n-1
     return 1 + min( reduceNumberTo1Naive(n - 1), reduceNumberTo1Naive(n + 1) )
+
+'''
+    Better way is, if n is even then divide it by 2, if n is odd, then when it is divided by 4 the possible remainder
+    can be 1 or 3, if remainder is 3, then it will make sense to add 1 to it (e.g if n = 19, n % 4 == 3),
+    by adding one it will be even and converge to 1 faster.
+    if remainder is 1, then it will converge to 1 faster when it is subtracted 1 from it. 
+'''
+
+def reduceNumberto1Optimal(n):
+    count = 0
+
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            # n == 3(special edge case), by this edge case it will be minimum else it will take one extra step 
+            # decrement by 1
+            if n % 4 == 1 or n == 3:
+                n -= 1
+
+            if n % 4 == 3:
+                n += 1
+        count += 1
+
+    return count
+
+n = 13
+print( reduceNumberTo1Naive(n) )
+print( reduceNumberto1Optimal(n) )
