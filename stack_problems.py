5ba5ae30a6326984e26243e712cdaf70d3669ab8
mahesh-keswani <karthikkeswani1234@gmail.com>
2021-03-13 18:40:40 +0530

    added example for maxstack and print function for it along with oldmax

--- stack_problems.py
+++ stack_problems.py
@@ -77,33 +77,50 @@ class MaxStack:
     def __init__(self):
         self.head = None
         self.max = None
-        
+
     def push(self, x):
         newNode = Node(x)
 
         if self.head is None:
             self.head = newNode
         else:
-            # setting the newnode as head
             newNode.next = self.head
             self.head = newNode
-
+            
         if self.max is None or newNode.value > self.max:
             newNode.oldMax = self.max
             self.max = newNode.value
 
     def pop(self):
+        if head is None:
+            return None
+        
+        node = self.head
+        self.head = self.head.next
+        node.next = None
+        # 2 -> 1
+        # 1    N
+        if node.value == self.max:
+            self.max = node.oldMax
+
+        return node.value
+
+    def maxStack(self):
+        return self.max
+
+    def printStack(self):
         if self.head is None:
             return None
 
-        temp = self.head
-        self.head = temp.next
-        temp.next = None
-
-        if temp.oldMax is not None:
-            self.max = temp.oldMax
+        node = self.head
+        while node:
+            print(node.value, node.oldMax)
+            node = node.next
             
-        return temp.value
+maxstack = MaxStack()
+maxstack.push(3)
+maxstack.push(7)
+maxstack.push(4)
+maxstack.push(5)
 
-    def maxValue(self):
-        return self.max
+maxstack.printStack()
